6d6c38ac426566ca2b812d5c6640e3c20be496e9
wq <guddqs@icloud.com>
2018-07-23 18:05:15 +0800

    转账分离

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -4,39 +4,45 @@ import './ArrayUtil.sol';
 
 contract Roulette {
     address public owner;
-    uint public randNonce = 0;
-    uint public buyCount = 0;
-    uint public allMoney = 0;
 
+    // --------  程序计数 ---------
+    uint public randNonce = 0; // 随机数(不安全)
+    uint public buyCount = 0;  // 购买数量
+    uint public allMoney = 0;  // 支付ether 总量统计
+
+    // --------  设定 ---------
+    uint payMoney = 1000000000;// 买key的钱: 1gwei
     uint ALL_CUSTOMER = 10; // 限定被买多少次开启转盘
     uint TIMES = 10;        // 设定转几次转盘
 
+
+    // --------  程序数据 ---------
     mapping(address => uint) public P;
     address [] public addresses;
     address public winner;
+    uint public status;     // 1.进行中  2.已结束
 
     constructor() public payable{
         owner = msg.sender;
+        status = 1;
     }
 
-    event fallbackTrigged(bytes data);
-
-    function() payable {fallbackTrigged(msg.data);}
-
-    function getBalance() constant returns (uint){
-        return this.balance;
+    function getBalance() constant returns (address){
+        return this;
     }
-
-    event SendEvent(address to, uint value, bool result);
 
     function buyKey() payable {
+        require(status == 1, 'Not running!');
+        require(msg.value == payMoney, 'You need pay 1 gwei!');
+        require(buyCount < ALL_CUSTOMER, 'Sell out, wait for open!');
         address key = msg.sender;
-        //TODO 买 key 的钱转到 owner
         uint money = msg.value;
-        bool result = owner.send(money);
+        owner.transfer(money);
         allMoney = allMoney + money;
-        SendEvent(owner, money, result);
         buyCount++;
+        if (buyCount == ALL_CUSTOMER) {
+            status = 2;
+        }
         bool exists = ArrayUtil.contains(addresses, key);
         if (exists) {
             P[key] = P[key] + p();
@@ -44,7 +50,6 @@ contract Roulette {
             addresses.push(key);
             P[key] = p();
         }
-        check();
     }
 
     function select() returns (uint){
@@ -56,23 +61,34 @@ contract Roulette {
         return 1;
     }
 
-    address [] public tempArray;
+    function check() payable returns (address){
+        address [] tempArray;
+        require(status == 2, 'Not Over!');
+        require(msg.sender == owner, 'Must from contract owner!');
+        require(msg.value == allMoney, 'You must pay all!');
 
-    function check() payable {
-        if (buyCount >= ALL_CUSTOMER) {
-            for (uint i = 0; i < TIMES; i++) {
-                address temp = roulette();
-                tempArray.push(temp);
-            }
-            uint random = uint(keccak256(now, msg.sender, randNonce)) % TIMES;
-            randNonce++;
-            winner = tempArray[random];
-            //TODO 把所有钱转给 winner
-//            bool result = winner.send(allMoney);
-//            SendEvent(winner, allMoney, result);
+        for (uint i = 0; i < TIMES; i++) {
+            address temp = roulette();
+            tempArray.push(temp);
         }
+        uint random = uint(keccak256(now, msg.sender, randNonce)) % TIMES;
+        randNonce++;
+        winner = tempArray[random];
+        //最后 用合约地址账户调用此方法
+        winner.transfer(msg.value);
+        status = 0;
+        clear();
+        temp = winner;
+        delete winner;
+        return temp;
     }
 
+    function clear() {
+        for (uint i = 0; i < addresses.length; i++) {
+            delete P[addresses[i]];
+        }
+        delete addresses;
+    }
 
     function roulette() returns (address) {
         uint random = uint(keccak256(now, msg.sender, randNonce)) % ALL_CUSTOMER;
